34205cad8b0c725ea38c64aa79f498de8a2ac5e9
Vikash Kumar <vikash.kumar@indoqubix.com>
2024-11-17 10:25:53 +0530

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -33,3 +33,85 @@ for ns in "${namespaces[@]}"; do
     aws s3 cp "$backup_file" "s3://$S3_BUCKET_NAME/$current_date/${ns}-backup-${current_date}.yaml"
 done
 
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Generate SSH key pair and save locally
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "local_file" "ssh_private_key" {
+  filename = "${path.module}/ec2-key.pem"
+  content  = tls_private_key.ssh_key.private_key_pem
+  file_permission = "0600"
+}
+
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "ec2-key"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+}
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get the default VPC's default subnet
+data "aws_subnet_ids" "default_subnets" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_subnet" "default_subnet" {
+  id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+}
+
+# Security group to allow SSH access
+resource "aws_security_group" "ssh_access" {
+  name_prefix = "allow_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = data.aws_vpc.default.id
+}
+
+# EC2 instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (example for us-west-2)
+  instance_type = "t2.micro"
+
+  key_name               = aws_key_pair.ec2_key.key_name
+  security_groups        = [aws_security_group.ssh_access.name]
+  subnet_id              = data.aws_subnet.default_subnet.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.example.public_ip
+}
+
+output "ssh_private_key_path" {
+  description = "Path to the private SSH key"
+  value       = local_file.ssh_private_key.filename
+}
+
